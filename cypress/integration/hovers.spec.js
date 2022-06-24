bc53850db10696ced47e4f9d3dbaa1e91c94a75b
SantiagoCabrales29 <sacamo96@gmail.com>
2022-06-24 10:10:02 -0500

    Adding tests for Hover Page

--- cypress/integration/hovers.spec.js
+++ cypress/integration/hovers.spec.js
@@ -5,23 +5,29 @@ describe("Hovers Test Page", () =>{
         cy.navigate("Hovers")
     })
     it("Hover over random profile", () => {
-        let value2 = Math.random() * 3;
-        cy.get('.figcaption').eq(0).invoke("show")
-        cy.get('.figcaption').find("h5").then(function(label){
-            //let value3 = label
-            cy.log(label.eq(0).text())
+        let value2 = Math.floor(Math.random() * 3);
+        cy.log(value2)
+        cy.get('.figcaption').eq(value2).invoke("show")
+        cy.get('.figcaption[style="display: block;"]').should('have.length',1)
+        cy.get('.figcaption').eq(value2).should('be.visible')
+        cy.get('.figcaption[style="display: block;"]').find('h5').then((displayedElement) => {
+            cy.log(displayedElement.text())
+            expect(displayedElement.text()).eq(`name: user${value2+1}`)
         })
-        //cy.contains('Top').click()
-        cy.get('.figcaption').find("h5").each(            
+    })
+
+    it.only("Hover over random profile 2", () => {
+        let value2 = Math.floor(Math.random() * 3);
+        cy.log(value2)
+        cy.get('.figcaption').eq(value2).invoke("show")
+        cy.get('.figcaption').each(            
             ($el,index,$list) => {
-        
-        }).then(($el) =>{
-            cy.log($el.text())
+               if($el.is(':visible')){
+                    cy.get($el).find('h5').then((displayedElement) => {
+                        cy.log(displayedElement.text())
+                        expect(displayedElement.text()).eq(`name: user${value2+1}`)
+                    })
+               } 
         })
-        
-    
-            //let value3 = label
-      //  cy.log(label.eq(0).text()),
-         
     })
 })
